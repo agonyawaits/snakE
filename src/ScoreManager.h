66e8529da6c3e787d1f937ec3040ba0774187679
Nikita Tokariev <mykyta.tokariev@gmail.com>
2020-04-28 13:38:25 +0300

    Modified ScoreManager, Snake

--- src/ScoreManager.h
+++ src/ScoreManager.h
@@ -2,19 +2,23 @@
 //  snake
 //  Copyright © 2019 Nikita Tokariev. All rights reserved.
 #pragma once
+#include "Vector2.hpp"
 
 class ScoreManager {
 public:
     ScoreManager();
 
     inline int getScore() const { return m_score; };
+    inline int getHighScore() const { return m_highScore; };
 
     void updateScore( const int& );
-    void printScore() const;
-    int getLastHighScore() const;
+    void printScores( const Vector2i&, const Vector2i& ) const;
     void logNewHighScore( const int& ) const;
 
 private:
     int m_score;
+    int m_highScore;
     int m_step;
+
+    int getLastHighScore() const;
 };
