29d2198ce5bb85306078bb08aee533ef57d144d2
Nikita T <mykyta.tokariev@gmail.com>
2021-04-17 15:34:55 +0300

    fix quickfix list

--- build/build.bash
+++ build/build.bash
@@ -1,10 +1,8 @@
 #!/bin/bash
-ARGS=$@
 LIBS="-lncurses"
 
 mkdir -p ${PROJECT_DIR}/bin/
-(cd ${PROJECT_DIR}/src/ &&
- g++ -Wall -g *.cpp -o ${PROJECT_NAME} -I${PROJECT_DIR}/include/ -std=c++2a ${ARGS} ${LIBS} &&
+(g++ -Wall -g ${PROJECT_DIR}/src/*.cpp -o ${PROJECT_NAME} -I${PROJECT_DIR}/include/ -std=c++2a ${LIBS} &&
  mv ./${PROJECT_NAME} ${PROJECT_DIR}/bin/ &&
  echo "build succeeded") || 
 echo "build failed"
